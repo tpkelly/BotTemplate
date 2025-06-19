16716c5333655b29c1f04c2d4032952ed87a5bcc
kazenone <kazenone@gmail.com>
2025-06-19 23:35:35 +0100

    Deleted lines from the wrong repository ._.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,3 @@
-const setup = require('../setupModal.js');
-const common = require('../common.js');
 const { Events } = require('discord.js');
 
 function noSuchCommand(client, interaction) {
@@ -35,75 +33,13 @@ async function commandInteraction(interaction, client) {
 }
 
 async function componentInteraction(interaction, client) {
-  if (interaction.customId === 'roulette-join') {
-    await interaction.deferReply({ ephemeral: true });
-    await common.register(interaction);
-    return;
-  }
-
   await interaction.deferUpdate();
   
   // Change pages on Setup command
   switch (interaction.customId) {
-    case 'config-frequency-prev':
-      await setup.channelPage(interaction);
-      return;
-      
-    case 'config-channel-next':
-    case 'config-mode-prev':
-      await setup.frequencyPage(interaction);
-      return;
-
-    case 'config-frequency-next':
-    case 'config-date-prev':
-      await setup.modePage(interaction);
-      return;
-
-    case 'config-mode-next':
-    case 'config-role-prev':
-      await setup.datePage(interaction);
-      return;
-
-    case 'config-date-next':
-    case 'config-summary-prev':
-      await setup.rolePage(interaction);
-      return;
-
-    case 'config-role-next':
-      await setup.summaryPage(interaction);
-      return;
-      
-    case 'config-finish':
-      await setup.finish(interaction);
-      return;
-
-    // Set setup data
-    case 'roulette-channel':
-      await setup.setChannel(interaction);
-      return;
-
-    case 'roulette-frequency':
-      await setup.setFrequency(interaction);
-      return;
-
-    case 'roulette-mode':
-      await setup.setMode(interaction);
-      return;
-      
-    case 'roulette-role':
-      await setup.setRole(interaction);
-      return;
-    
-    case 'config-role-new':
-      await setup.newRole(interaction);
-      return;
+    default: break;
   }
   
-  if (interaction.customId.startsWith('config-date-set-')) {
-    interaction.values = [ interaction.customId.substring(18) ];
-    await setup.setDate(interaction);
-    return;
-  }
 
   throw `Unknown interaction: ${interaction.customId}`
 }
